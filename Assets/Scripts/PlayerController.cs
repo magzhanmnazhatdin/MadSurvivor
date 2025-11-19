7f531615070e62bc84435b11d61e3ec389411d5c
magzhanmnazhatdin <79352934+magzhanmnazhatdin@users.noreply.github.com>
2025-11-19 18:36:45 +0500

    implemented player controller, animator

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+
     [SerializeField] private float moveSpeed;
+    public Vector3 playerMoveDirection;
 
     // Update is called once per frame
     void Update()
     {
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
+        playerMoveDirection = new Vector3(inputX, inputY).normalized;
 
-        rb.linearVelocity = new Vector2(inputX, inputY).normalized;
+        animator.SetFloat("moveX", inputX);
+        animator.SetFloat("moveY", inputY);
+
+        if (playerMoveDirection == Vector3.zero)
+        {
+            animator.SetBool("moving", false);
+        } else {
+            animator.SetBool("moving", true);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(playerMoveDirection.x * moveSpeed, playerMoveDirection.y * moveSpeed);
     }
 }
